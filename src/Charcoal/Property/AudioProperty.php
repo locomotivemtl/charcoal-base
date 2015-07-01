18e0009bb4e4a50f5205fdd4ca45ba51cc5cc538
Mathieu Ducharme <mat@locomotive.ca>
2015-07-01 18:23:22 -0400

    Add min length and max length to audio property. Allow mp3 (audio/mpeg) files.

--- src/Charcoal/Property/AudioProperty.php
+++ src/Charcoal/Property/AudioProperty.php
@@ -2,11 +2,27 @@
 
 namespace Charcoal\Property;
 
+use \InvalidArgumentException as InvalidArgumentException;
+
 // From `charcoal-admin`
 use \Charcoal\Property\FileProperty as FileProperty;
 
 class AudioProperty extends FileProperty
 {
+
+
+    /**
+    * Minimum audio length, in seconds.
+    * @var integer $_min_length
+    */
+    private $_min_length = 0;
+
+    /**
+    * Maximum audio length, in seconds.
+    * @var integer $_max_length
+    */
+    private $_max_length = 0;
+
     /**
     * @return string
     */
@@ -16,11 +32,74 @@ class AudioProperty extends FileProperty
     }
 
     /**
+    * @param array $data
+    * @return AudioProperty Chainable
+    */
+    public function set_data($data)
+    {
+
+        parent::set_data($data);
+
+        if (isset($data['min_length']) && $data['min_length'] !== null) {
+            $this->set_min_length($data['min_length']);
+        }
+        if (isset($data['max_length']) && $data['max_length'] !== null) {
+            $this->set_max_length($data['max_length']);
+        }
+        return $this;
+    }
+
+    /**
+    * @param integer $min_length
+    * @throws InvalidArgumentException
+    * @return AudioProperty Chainable
+    */
+    public function set_min_length($min_length)
+    {
+        if (!is_int($min_length)) {
+            throw new InvalidArgumentException('Min length must be an integer (in seconds)');
+        }
+        $this->_min_length = $min_length;
+        return $this;
+    }
+
+    /**
+    * @return integer
+    */
+    public function min_length()
+    {
+        return $this->_min_length;
+    }
+
+    /**
+    * @param integer $max_length
+    * @throws InvalidArgumentException
+    * @return AudioProperty Chainable
+    */
+    public function set_max_length($max_length)
+    {
+        if (!is_int($max_length)) {
+            throw new InvalidArgumentException('Max length must be an integer (in seconds)');
+        }
+        $this->_max_length = $max_length;
+        return $this;
+    }
+
+    /**
+    * @return integer
+    */
+    public function max_length()
+    {
+        return $this->_max_length;
+    }
+
+    /**
     * @return array
     */
     public function accepted_mimetypes()
     {
         return [
+            'audio/mpeg',
             'audio/wav',
             'audio/x-wav'
         ];
@@ -35,6 +114,10 @@ class AudioProperty extends FileProperty
         
         $ext = '';
         switch($mimetype) {
+            case 'audio/mpeg':
+                $ext = 'mp3';
+            break;
+
             case 'audio/wav':
             case 'audio/x-wav':
                 $ext = 'wav';
---
+++ tests/Charcoal/Property/AudioPropertyTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Charcoal\Tests\Property;
+
+use \Charcoal\Property\AudioProperty as AudioProperty;
+
+/**
+* ## TODOs
+* - 2015-03-12:
+*/
+class AudioPropertyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+    * Hello world
+    */
+    public function testConstructor()
+    {
+        $obj = new AudioProperty();
+        $this->assertInstanceOf('\Charcoal\Property\AudioProperty', $obj);
+
+        $this->assertEquals(0, $obj->min_length());
+        $this->assertEquals(0, $obj->max_length());
+    }
+
+    public function testType()
+    {
+        $obj = new AudioProperty();
+        $this->assertEquals('audio', $obj->type());
+    }
+
+    public function testSetData()
+    {
+        $obj = new AudioProperty();
+        $data = [
+            'min_length'=>20,
+            'max_length'=>500
+        ];
+        $ret = $obj->set_data($data);
+        $this->assertSame($ret, $obj);
+
+        $this->assertEquals(20, $obj->min_length());
+        $this->assertEquals(500, $obj->max_length());
+    }
+
+    public function testSetMinLength()
+    {
+        $obj = new AudioProperty();
+
+        $ret = $obj->set_min_length(5);
+        $this->assertSame($ret, $obj);
+
+        $this->assertEquals(5, $obj->min_length());
+
+        $this->setExpectedException('\InvalidArgumentException');
+        $obj->set_min_length(false);
+    }
+
+    public function testSetMaxLength()
+    {
+        $obj = new AudioProperty();
+
+        $ret = $obj->set_max_length(5);
+        $this->assertSame($ret, $obj);
+
+        $this->assertEquals(5, $obj->max_length());
+
+        $this->setExpectedException('\InvalidArgumentException');
+        $obj->set_max_length(false);
+    }
+
+    /**
+    * @dataProvider mimeExtensionProvider
+    */
+    public function testGenerateExtension($mime, $ext)
+    {
+        $obj = new AudioProperty();
+        $obj->set_mimetype($mime);
+        $this->assertEquals($ext, $obj->generate_extension());
+    }
+
+    public function mimeExtensionProvider()
+    {
+        return [
+            ['audio/mpeg', 'mp3'],
+            ['audio/wav', 'wav'],
+            ['audio/x-wav', 'wav']
+        ];
+    }
+
+}
