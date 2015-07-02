83a775ce5dcef92e4d37ebffebadf9a02e9bd574
Mathieu Ducharme <mat@locomotive.ca>
2015-07-02 12:34:53 -0400

    Run phpunit from xml file. Allow failure on hhvm for travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,20 +48,11 @@ module.exports = function(grunt) {
 		},
 
 		phpunit:{
-
 			src: {
 				dir: 'tests/'
 			},
-
 			options: {
-				colors: true,
-				coverageHtml:'tests/tmp/report/',
-				//coverageText:'tests/tmp/report/',
-				testdoxHtml:'tests/tmp/testdox.html',
-				testdoxText:'tests/tmp/testdox.text',
-				verbose:true,
-				debug:false,
-				bootstrap:'tests/bootstrap.php'
+				configuration: 'tests/phpunit.xml'
 			}
 		},
 
