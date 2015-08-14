87cbe8a2cf927d807364c4f0b284ed820c265ad6
Mathieu Ducharme <mat@locomotive.ca>
2015-08-14 12:05:17 -0400

    CategoryTrait / CategoryInterface

--- src/Charcoal/Object/CategoryInterface.php
+++ src/Charcoal/Object/CategoryInterface.php
@@ -4,5 +4,31 @@ namespace Charcoal\Model;
 
 interface CategoryInterface
 {
-    // ...
+    /**
+    * @param array $data
+    * @return CategoryInterface Chainable
+    */
+    public function set_category_data(array $data);
+
+    /**
+    * @return string
+    */
+    public function item_type();
+
+    /**
+    * Get the number of items in this category.
+    * @param array
+    */
+    public function num_items();
+
+    /**
+    * @return boolean
+    */
+    public function has_items();
+
+    /**
+    * @return array
+    * @todo Return Collection?
+    */
+    public function items();
 }
--- src/Charcoal/Object/CategoryTrait.php
+++ src/Charcoal/Object/CategoryTrait.php
@@ -2,7 +2,41 @@
 
 namespace Charcoal\Model;
 
+/**
+*
+*/
 trait CategoryTrait
 {
-    // ...
+
+    /**
+    * @param array
+    */
+    public function set_category_data(array $data)
+    {
+        return $this;
+    }
+
+    /**
+    * @return string
+    */
+    abstract public function item_type();
+
+    /**
+    * @return integer
+    */
+    abstract public function num_items();
+
+    /**
+    * @return boolean
+    */
+    public function has_items()
+    {
+        $num_items = $this->num_items();
+        return ($num_items > 0);
+    }
+
+    /**
+    * @return array
+    */
+    abstract public function items();
 }
