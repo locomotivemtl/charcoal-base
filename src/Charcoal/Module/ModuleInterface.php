317ebcdb70f47bf3b6cb64758210951d14324187
Mathieu Ducharme <mat@locomotive.ca>
2015-08-20 15:06:52 -0400

    Remove Module Interface

--- src/Charcoal/Module/ModuleInterface.php
+++ src/Charcoal/Module/ModuleInterface.php
@@ -9,15 +9,18 @@ interface ModuleInterface
     * @param array $data
     * @return ModuleInterface Chainable
     */
-    public function init(array $data = null);
+    //public function init(array $data = null);
+
+    //setup
+    //default_route
 
     /**
     * @return ModuleInterface Chainable
     */
-    public function setup_routes();
+    //public function setup_routes();
 
     /**
     * @return ModuleInterface Chainable
     */
-    public function setup_cli_routes();
+    //public function setup_cli_routes();
 }
