d2aed99374cd2808fec3cdb81940f0c62913ca52
Mathieu Ducharme <mat@locomotive.ca>
2015-09-28 00:00:49 -0400

    Full unit tests for boolean property.

--- tests/Charcoal/Property/BooleanPropertyTest.php
+++ tests/Charcoal/Property/BooleanPropertyTest.php
@@ -22,6 +22,45 @@ class BooleanPropertyTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('boolean', $obj->type());
     }
 
+    public function testDisplayVal()
+    {
+        $obj = new BooleanProperty();
+        $this->assertEquals('False', $obj->display_val());
+        $obj->set_val(true);
+        $this->assertEquals('True', $obj->display_val());
+
+        $obj->set_true_label('Oui');
+        $obj->set_false_label('Non');
+
+        $this->assertEquals('Oui', $obj->display_val(true));
+        $this->assertEquals('Non', $obj->display_val(false));
+
+
+    }
+
+    /**
+    * Assert that the `set_multiple()` method:
+    * - set the multiple to false, if false or falsish value
+    * - throws exception otherwise (truthish or invalid value)
+    * - is chainable
+    */
+    public function testSetMultiple()
+    {
+        $obj = new BooleanProperty();
+        $ret = $obj->set_multiple(0);
+        $this->assertSame($ret, $obj);
+        $this->assertSame(false, $ret->multiple());
+
+        $this->setExpectedException('\InvalidArgumentException');
+        $obj->set_multiple(1);
+    }
+
+    public function testMultiple()
+    {
+        $obj = new BooleanProperty();
+        $this->assertSame(false, $obj->multiple());
+    }
+
     public function testSetData()
     {
         $obj = new BooleanProperty();
@@ -61,6 +100,12 @@ class BooleanPropertyTest extends \PHPUnit_Framework_TestCase
         //$obj->set_false_label(false);
     }
 
+    public function testSqlExtra()
+    {
+        $obj = new BooleanProperty();
+        $this->assertSame('', $obj->sql_extra());
+    }
+
     public function testSqlType()
     {
         $obj = new BooleanProperty();
@@ -71,5 +116,37 @@ class BooleanPropertyTest extends \PHPUnit_Framework_TestCase
     {
         $obj = new BooleanProperty();
         $this->assertEquals(\PDO::PARAM_BOOL, $obj->sql_pdo_type());
+    }
+
+    public function testChoices()
+    {
+        $obj = new BooleanProperty();
+        $obj->set_val(false);
+        $choices = [
+            [
+                'label'=>'True',
+                'selected'=>false,
+                'value'=>1
+            ],
+            [
+                'label'=>'False',
+                'selected'=>true,
+                'value'=>0
+            ]
+        ];
+        $this->assertEquals($choices, $obj->choices());
+        
+    }
+
+    public function testSave()
+    {
+        $obj = new BooleanProperty();
+        
+        $obj->set_val(true);
+        $this->assertTrue($obj->save());
+
+        $obj->set_val(false);
+        $this->assertNotTrue($obj->save()); 
+
     }
 }
