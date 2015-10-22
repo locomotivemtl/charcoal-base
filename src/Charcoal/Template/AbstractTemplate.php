0dfad95257d00795a2620f627eb204ea50bcd4c9
Mathieu Ducharme <mat@locomotive.ca>
2015-10-22 18:21:09 -0400

    Add logger to template. Remove unnecessary READMEs.

--- src/Charcoal/Template/AbstractTemplate.php
+++ src/Charcoal/Template/AbstractTemplate.php
@@ -2,6 +2,10 @@
 
 namespace Charcoal\Template;
 
+// PSR-3 logger
+use \Psr\Log\LoggerInterface;
+use \Psr\Log\LoggerAwareInterface;
+
 // Module `charcoal-core` dependencies
 use \Charcoal\Model\AbstractModel as AbstractModel;
 use \Charcoal\View\ViewableInterface as ViewableInterface;
@@ -14,18 +18,60 @@ use \Charcoal\Template\TemplateInterface as TemplateInterface;
 *
 */
 abstract class AbstractTemplate implements
+    LoggerAwareInterface,
     TemplateInterface,
     ViewableInterface
 {
 
     use ViewableTrait;
 
+    /**
+    * @var LoggerInterface $logger
+    */
+    private $logger;
+
     public function __construct(array $data = null)
     {
+        if (isset($data['logger'])) {
+            $this->set_logger($data['logger']);
+        }
+
         if ($data !== null) {
             $this->set_data($data);
         }
     }
+
+    /**
+    * > LoggerAwareInterface > setLogger()
+    *
+    * Fulfills the PSR-1 style LoggerAwareInterface
+    *
+    * @param LoggerInterface $logger
+    * @return AbstractEngine Chainable
+    */
+    public function setLogger(LoggerInterface $logger)
+    {
+        return $this->set_logger($logger);
+    }
+
+    /**
+    * @param LoggerInterface $logger
+    * @return AbstractEngine Chainable
+    */
+    public function set_logger(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger;
+        return $this;
+    }
+
+    /**
+    * @erturn LoggerInterface
+    */
+    public function logger()
+    {
+        return $this->logger;
+    }
+
 
     public function create_view(array $data = null)
     {
