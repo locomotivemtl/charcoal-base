58e5592a784d5c7f57dc0db9de27d436b08aa27b
beneroch <roch.bene@gmail.com>
2015-12-07 17:48:04 -0500

    Added check so that password don't get hashed multiple times

--- src/Charcoal/Property/PasswordProperty.php
+++ src/Charcoal/Property/PasswordProperty.php
@@ -24,8 +24,13 @@ class PasswordProperty extends StringProperty
     {
         $pw_opts = ['cost'=>12];
         $password = $this->val();
-        $val = password_hash($password, PASSWORD_DEFAULT, $pw_opts);
-        $this->set_val($val);
+        $val = $this->val();
+
+        // Assuming the password_needs_rehash is set to true is the hash given isn't a hash
+        if (password_needs_rehash($password, PASSWORD_DEFAULT, $pw_opts)) {
+            $val = password_hash($password, PASSWORD_DEFAULT, $pw_opts);
+            $this->set_val($val);
+        }
 
         return $val;
     }
