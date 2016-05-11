0ba111b8e3f85cbd0d0a128653544f5c684cbdb2
Mathieu Ducharme <mat@locomotive.ca>
2016-05-11 17:30:39 -0400

    Revert "Simplify / improve the slugify method."
    This reverts commit 6af2a9ce8f98a08b953da261f582ca3d9b9d946b.

--- src/Charcoal/Object/RoutableTrait.php
+++ src/Charcoal/Object/RoutableTrait.php
@@ -121,34 +121,52 @@ trait RoutableTrait
      */
     public function slugify($str)
     {
+        // Character options
+        $separator = '_';
+        $punctuation_modifier = $separator;
+
+        // Punctuation
+        $punctuation_characters = ['&', '%', '?', ')', '(', '\\', '"', "'", ':', '#', '.', ',', ';', '!'];
+
+        // Unescaped HTML characters string
+        $unescaped_html_characters = '/&(raquo|laquo|rsaquo|lsaquo|rdquo|ldquo|rsquo|lsquo|hellip|amp|nbsp|quot|ordf|ordm);/';
 
         $separator = '-';
 
-        $slug = preg_replace('/[^\p{L}\s]/u', $separator, $str);
+        $slug = preg_replace('/[^\p{L}\s]/u', '-', $str);
 
         $slug = mb_strtolower($slug, 'UTF-8');
 
         // Strip HTML
         $slug = strip_tags($slug);
 
+        // Strip Whitespace
+        $slug = trim($slug);
+
         // Remove diacritics
         $slug = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde|cedil|ring);/', '$1', htmlentities($slug, ENT_COMPAT, 'UTF-8'));
 
         // Remove unescaped HTML characters
-        $unescaped = '/&(raquo|laquo|rsaquo|lsaquo|rdquo|ldquo|rsquo|lsquo|hellip|amp|nbsp|quot|ordf|ordm);/';
-        $slug = preg_replace($unescaped, '', $slug);
+        $slug = preg_replace($unescaped_html_characters, '', $slug);
+
+        // Get rid of punctuation
+        $slug = str_replace($punctuation_characters, $separator, $slug);
+
+        // Post-cleanup, get rid of spaces, repeating dash symbols symbols and surround whitespace/separators
+        $slug = trim($slug);
 
         // Replace whitespace by seperator
-        $slug = preg_replace('/\s+/', '-', $slug);
+        $slug = preg_replace('/\s+/', $separator, $slug);
 
         // Squeeze multiple dashes or underscores
-        $slug = preg_replace('/[-]{2,}/', '-', $slug);
+        $slug = preg_replace('/[-_]{2,}/', '-', $slug);
 
         // Strip leading and trailing dashes or underscores
-        $slug = trim($slug, '-');
+        $slug = trim($slug, '-_');
 
         // Finally, remove all whitespace
-        $slug = preg_replace('/[_]+/', '-', $slug);
+        $slug = preg_replace('/[_]+/', $separator, $slug);
+        //$slug = str_replace('_', $separator, $slug);
 
         return $slug;
     }
