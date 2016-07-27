ff7f4de54875a5dd0a7232b82d55af54d30d31c7
Mathieu Ducharme <mat@locomotive.ca>
2016-07-27 08:41:29 -0400

    Fix schedule interface

--- src/Charcoal/Object/ObjectScheduleInterface.php
+++ src/Charcoal/Object/ObjectScheduleInterface.php
@@ -9,7 +9,7 @@ interface ObjectScheduleInterface
 {
     /**
      * @param string $targetType The object type (type-ident).
-     * @return \Charcoal\Object\ObjectRevisionInterface Chainable
+     * @return ObjectScheduleInterface Chainable
      */
     public function setTargetType($targetType);
 
@@ -20,7 +20,7 @@ interface ObjectScheduleInterface
 
     /**
      * @param mixed $targetId The object ID.
-     * @return \Charcoal\Object\ObjectRevisionInterface Chainable
+     * @return ObjectScheduleInterface Chainable
      */
     public function setTargetId($targetId);
 
@@ -34,20 +34,20 @@ interface ObjectScheduleInterface
      *
      * @param  null|string|DateTimeInterface $ts A date/time string or object.
      * @throws InvalidArgumentException If the date/time is invalid.
-     * @return QueueItemInterface Chainable
+     * @return ObjectScheduleInterface Chainable
      */
     public function setScheduledDate($ts);
 
     /**
      * Retrieve the date/time the item should be processed at.
      *
-     * @return null|DateTimeInterface
+     * @return null|\DateTimeInterface
      */
     public function scheduledDate();
 
     /**
      * @param array|string $data The data diff.
-     * @return ObjectRevision
+     * @return ObjectScheduleInterface Chainable
      */
     public function setDataDiff($data);
 
@@ -61,14 +61,28 @@ interface ObjectScheduleInterface
      *
      * @param  null|string|DateTimeInterface $ts A date/time string or object.
      * @throws InvalidArgumentException If the date/time is invalid.
-     * @return QueueItemInterface Chainable
+     * @return ObjectScheduleInterface Chainable
      */
     public function setProcessedDate($ts);
 
     /**
      * Retrieve the date/time the item was processed at.
      *
-     * @return null|DateTimeInterface
+     * @return null|\DateTimeInterface
      */
     public function processedDate();
+
+    /**
+     * Process the item.
+     *
+     * @param  callable $callback        An optional callback routine executed after the item is processed.
+     * @param  callable $successCallback An optional callback routine executed when the item is resolved.
+     * @param  callable $failureCallback An optional callback routine executed when the item is rejected.
+     * @return boolean  Success / Failure
+     */
+    public function process(
+        callable $callback = null,
+        callable $successCallback = null,
+        callable $failureCallback = null
+    );
 }
