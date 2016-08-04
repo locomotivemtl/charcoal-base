7afc0d8df21e21a958cdbee0f15cc06cba4e29f3
Chauncey McAskill <chauncey@locomotive.ca>
2016-08-04 18:15:07 -0400

    Fixed uniqueness of ObjectRoute
    Changes:
    - Cleaned up class and JSON metadata;
    - Added ‘ObjectRouteInterface’, just in case it could be useful;
    - Added ‘model/collection/loader’ to ObjectRoute;
    - Renamed ‘ObjectRoute::generateUniqueSlugFromCurrentObjectSlug()’ to ‘ObjectRoute::generateUniqueSlug()’;
    - Revised ‘ObjectRoute::isSlugUnique()’ to use CollectionLoader and better handle checks and occurrences of self;
    - Fixed ‘ObjectRoute::generateUniqueSlug()’ to properly append unique-suffix to original slug;
    - Improved documentation of properties and methods;

---
+++ src/Charcoal/Object/ObjectRouteInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Charcoal\Object;
+
+/**
+ * Defines a route to an object implementing {@see \Charcoal\Object\RoutableInterface}.
+ *
+ * {@see \Charcoal\Object\ObjectRoute} for a basic implementation.
+ */
+interface ObjectRouteInterface
+{
+}
